8c5ae6cc5908c612b0054ac5f2300d198f85326b
Rabin Tamang <lrabin935@gmail.com>
2025-12-27 15:10:18 +0545

    login done without image

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../_components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className=" flex items-center justify-center">
       <LoginForm />
     </div>
   );
