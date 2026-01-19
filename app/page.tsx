d528ee7ddb3f7775728b225eed99f65d062fca44
Rabin Tamang <lrabin935@gmail.com>
2026-01-19 18:44:06 +0545

    new

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./_components/navbar";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-zinc-50 dark:bg-white">
+      {/* <Navbar /> */}
+    </div>
+  );
+}
